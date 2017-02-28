4a67ba62768830a0474339a04e96e62f3136634f
PalentinoSirait <valen.sirait@gmail.com>
2017-02-28 15:49:04 +0700

    Program Menampilkan identitas mahasiswa

---
+++ Minggu 2/mahasiswa.java
@@ -0,0 +1,7 @@
+public class mahasiswa{
+	public static void main(String argv[]){
+		System.out.println("Nama   : Palentino Sirait");
+		System.out.println("NIM    : 10113800");
+		System.out.println("Alamat : Bandung");
+	}
+}
