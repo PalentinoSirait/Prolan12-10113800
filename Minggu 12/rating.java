17ac80d41225fc493e376d5f522373471faf878a
PalentinoSirait <valen.sirait@gmail.com>
2017-05-30 23:49:49 +0700

    Implentasi kelas generik dan penambahan kelas rating serta fitur rating di line 26 - 35 di mainClass

---
+++ Minggu 12/rating.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author valen
+ */
+public class rating <T> {
+    T judulLagu;
+    T rat;
+
+    public T getJudulLagu() {
+        return judulLagu;
+    }
+
+    public T getRat() {
+        return rat;
+    }
+
+    public void setJudulLagu(T judulLagu) {
+        this.judulLagu = judulLagu;
+    }
+
+    public void setRat(T rat) {
+        this.rat = rat;
+    }
+    
+}
